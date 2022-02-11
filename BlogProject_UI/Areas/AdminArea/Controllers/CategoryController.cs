2a338c94dbd30637554f680c4d2048565bd82cb9
Emreedk <emreedk@gmail.com>
2022-02-11 17:49:20 +0300

    Admin tarafinda CRUD islemleri

--- BlogProject_UI/Areas/AdminArea/Controllers/CategoryController.cs
+++ BlogProject_UI/Areas/AdminArea/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BlogProject_BusinessLayer.Concrete;
+using BlogProject_BusinessLayer.ValidationRules;
 using BlogProject_DataAccessLayer.EntityFramework;
+using BlogProject_EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,6 +21,38 @@ namespace BlogProject_UI.Areas.AdminArea.Controllers
         {
             var values = cm.GetList().ToPagedList(page,3);
             return View(values);
+        }
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult res = cv.Validate(category);
+            if (res.IsValid)
+            {
+                category.CategoryStatus = true;
+                cm.Add(category);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in res.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = cm.GetById(id);
+            cm.Delete(value);
+            return RedirectToAction("Index", "Category");
         }
     }
 }
--- BlogProject_UI/Controllers/AdminController.cs
+++ BlogProject_UI/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,13 @@ using System.Threading.Tasks;
 
 namespace BlogProject_UI.Controllers
 {
+    
     public class AdminController : Controller
     {
         public IActionResult Index()
         {
             return View();
+
         }
 
         public PartialViewResult AdminNavbarPartial()
