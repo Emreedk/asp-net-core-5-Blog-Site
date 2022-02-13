d50e64f372d2c5db80b81f2871bda5b0adb5a26d
Emreedk <emreedk@gmail.com>
2022-02-13 18:08:19 +0300

    Admin kontrollerinde duzenlemeler ve rapor closedXML ile raporlama islemi

---
+++ BlogProject_UI/Areas/AdminArea/Controllers/BlogController.cs
@@ -0,0 +1,100 @@
+﻿using BlogProject_DataAccessLayer.Concrete;
+using BlogProject_UI.Areas.AdminArea.Models;
+using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject_UI.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class BlogController : Controller
+    {
+        public IActionResult ExportStaticExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog Id";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;  //ilk satıra blog id ve blog adını yazdırdığım için 2. satırdan başladım.
+                foreach (var item in GetBlogList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.Id;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+                using(var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Calisma1.xlsx");
+                }
+            }
+            
+        }
+
+        public List<BlogModel> GetBlogList()
+        {
+            List<BlogModel> bm = new List<BlogModel>
+            {
+                new BlogModel{Id = 1 , BlogName="C# Programlamaya Giriş" },
+                new BlogModel{Id = 2 , BlogName = "Tesla Firma Araçları"},
+                new BlogModel{Id = 3 , BlogName= "2020 Olimpiyatları"}
+            };
+            return bm;
+        }
+
+        public IActionResult BlogListExcel()
+        {
+            return View();
+        }
+
+        public IActionResult ExportDynamicExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog Id";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;  //ilk satıra blog id ve blog adını yazdırdığım için 2. satırdan başladım.
+                foreach (var item in BlogTitleList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.Id;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Blog Raporu.xlsx");
+                }
+            }
+        }
+
+        public List<BlogModel2> BlogTitleList()
+        {
+            List<BlogModel2> bm = new List<BlogModel2>();
+            using( var c = new Context())
+            {
+                bm = c.Blogs.Select(x => new BlogModel2
+                {
+                    Id = x.Id,
+                    BlogName = x.BlogTitle
+                }).ToList();
+            }
+            return bm;
+        }
+
+        public IActionResult BlogTitleListExcel()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlogProject_UI/Areas/AdminArea/Controllers/WidgetController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject_UI.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class WidgetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
